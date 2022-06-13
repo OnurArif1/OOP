492f802040e34f671d186a36720f0a83880601da
Onur Arif Ciftci <onurarifciftci@gmail.com>
2022-06-13 17:56:06 +0300

    ekleme

---
+++ OOP/Encapsulation/Dikdortgen.cs
@@ -0,0 +1,36 @@
+using System;
+public class Dikdortgen
+{
+    private int boy;
+    public int Boy
+    {
+        get{return boy;}
+        set
+        {
+            if(value<0)
+            {
+                boy=0;
+            }
+            else boy=value;
+        }
+    }
+    private int en;
+    public int En 
+    {
+        get{return en;}
+        set
+        {
+            if(value<0)
+            {
+                en=0;
+            }
+            else en=value;
+        }
+    }
+    public int Alan()
+    {
+        int sonuc=0;
+        sonuc=en*boy;
+        return sonuc;
+    }
+}
---
+++ OOP/Encapsulation/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var d = new Dikdortgen();
+            d.Boy =Convert.ToInt32(Console.ReadLine());
+            d.En = Convert.ToInt32(Console.ReadLine());
+            
+ 
+            Console.WriteLine("Boy:" + d.Boy + "En:" + d.En);
+            
